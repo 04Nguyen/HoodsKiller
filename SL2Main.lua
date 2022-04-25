aa9a5cafa0f6784187d3d1faa80bba40434a1151
NguyenChuChe <84752675+NguyenChuChe@users.noreply.github.com>
2022-04-26 01:21:54 +0700

    Update SL2Main.lua

--- SL2Main.lua
+++ SL2Main.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Hoods Killer"})
+local Window = OrionLib:MakeWindow({Name = "Hoods Killer | SL2"})
 
 local Main = Window:MakeTab({
 	Name = "Main",
