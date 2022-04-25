f51c4a1a2c695fa9b4068b3bb556dacdf5fbf16c
NguyenChuChe <84752675+NguyenChuChe@users.noreply.github.com>
2022-04-26 01:22:19 +0700

    Create EB.lua

---
+++ EB.lua
@@ -0,0 +1,18 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Hoods Killer | EB"})
+
+local Main = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Players = Window:MakeTab({
+	Name = "Players",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Misc = Window:MakeTab({
+	Name = "Miscellaneous",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
