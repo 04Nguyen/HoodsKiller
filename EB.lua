3de1e466b3e858328f8b155a268c5d13c2279594
NguyenChuChe <84752675+NguyenChuChe@users.noreply.github.com>
2022-04-26 17:34:49 +0700

    Update EB.lua

--- EB.lua
+++ EB.lua
@@ -16,3 +16,24 @@ local Misc = Window:MakeTab({
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+
+Misc:AddButton({
+	Name = "Switch Game",
+	Callback = function()
+      		print("button pressed")
+			  if game.PlaceId == 4779613061 then
+				game:GetService("TeleportService"):Teleport(1923555882, game:GetService("Players").LocalPlayer)
+			  elseif game.PlaceId == 1923555882 then
+				game:GetService("TeleportService"):Teleport(4779613061, game:GetService("Players").LocalPlayer)
+			  end
+  	end    
+})
+
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+	if State == Enum.TeleportState.Started then
+		syn.queue_on_teleport("sex")
+	end
+end)
+
+OrionLib:Init()
+
